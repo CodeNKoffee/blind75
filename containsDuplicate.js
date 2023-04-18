3501a9eb0697d1a4a21372228d87acc01c3db1a9
Hatem Soliman <hatemthedev@gmail.com>
2023-04-18 19:44:18 +0200

    Blind75 - 1

---
+++ containsDuplicate.js
@@ -0,0 +1,38 @@
+// Blind 75 - 1
+
+/**
+ * My Solution:
+ * Time: O(n^2)
+ * Space: O(1)
+ */
+// var containsDuplicate = function(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         let temp = nums[i];
+//         for (let j = 0; j < nums.length; j++) {
+//             if (i === j) {
+//                 j++;
+//             } else {
+//                 if (temp === nums[j]) {
+//                     return true;
+//                 }
+//             }
+//         }
+//     }
+//     return false
+// };
+
+/**
+ * Ideal Solution
+ * Time: O(n)
+ * Space: O(n)
+ */
+var containsDuplicate = function(nums) {
+    const set = new Set;
+    for (const num of nums) {
+        if (set.has(num)) {
+            return true;
+        }
+        set.add(num);
+    }
+    return false;
+}
